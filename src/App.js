36214a90b5ea273c58eba544e512553f45c77e8d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-03-21 14:04:18 -0700

    add openai import statement and create api key Confirmation object

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 import InputBox from './InputBox';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 function App() {
+
+
   return (
     <div>
       <InputBox />
