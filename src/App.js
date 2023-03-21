c5de953f5092163eb7a9715845b2655fca1695bd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-03-21 16:27:08 -0700

    add generateImage() that handles posting prompt, number, and size to the API and returns imageUrl. Add conditional to display imageUrl only if it exists.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai';
 const configuration = new Configuration({
   apiKey: process.env.REACT_APP_API_KEY,
 });
-
+console.log(process.env.REACT_APP_API_KEY)
 const openai = new OpenAIApi(configuration);
 
 function App() {
@@ -15,12 +15,25 @@ function App() {
   const [size, setSize] = useState("256x256");
   const [imageUrl, setImageUrl] = useState("");
 
+  const generateImage = async () => {
+    const imageParams = {
+      prompt: userPrompt,
+      n: parseInt(number),
+      size: size,
+    };
+    
+    const response = await openai.createImage(imageParams);
+    const urlData = response.data.data[0].url;
+    setImageUrl(urlData);
+  };
+
   return (
     <div>
+      {imageUrl && <img src={imageUrl} alt="ai generated image" /> }
       <InputBox label={"Description"} setAttribute={setUserPrompt} />
       <InputBox label={"Amount"} setAttribute={setNumber} />
       <InputBox label={"Size"} setAttribute={setSize} />
-      <button >Create</button>
+      <button onClick={generateImage}>Create</button>
     </div>
   );
 }
